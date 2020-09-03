55ff6141f3b823bb279496d851bf790c8b78acfa
juker <juker1141@gmail.com>
2020-09-03 17:19:31 +0800

    update

--- src/views/layout/Layout.vue
+++ src/views/layout/Layout.vue
@@ -1,10 +1,19 @@
 <template>
-  <div>
-    <router-link to="/home">Home</router-link>|
-    <router-link to="/about">About</router-link>|
-    <router-link to="/products">Products</router-link>|
-    <router-link to="/cart">Cart</router-link>|
-    <router-link to="/login">Admin</router-link>
+  <div class="container">
+    <navbar class="d-flex justify-content-between align-items-center my-3">
+      <button class="btn" type="button">
+        <span class="material-icons">
+        menu
+        </span>
+      </button>
+      <router-link class="fontOrbitron fz_30 text-black text-decoration-none" to="/home">
+      <div>
+        <router-link to="/products">Products</router-link>
+        <router-link to="/cart">Cart</router-link>
+        <router-link to="/about">About</router-link>
+        <router-link to="/login">Admin</router-link>
+      </div>
+    </navbar>
     <hr />
     <router-view class="container" />
   </div>
