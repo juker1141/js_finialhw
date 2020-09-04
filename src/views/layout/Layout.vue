476ac2874c86166b6d6537ec402e6699aef4fe56
juker <juker1141@gmail.com>
2020-09-04 17:16:46 +0800

    update

--- src/views/layout/Layout.vue
+++ src/views/layout/Layout.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="container h_100vh">
     <navbar class="d-flex justify-content-between align-items-center my-3 px-2 position-relative">
-      <button class="btn p-1 justify-content-between align-items-center d-none" type="button">
+      <button class="btn p-1 justify-content-between align-items-center d-flex d-lg-none"
+      type="button" @click="navbarShow">
         <span class="material-icons">menu</span>
       </button>
       <router-link
@@ -12,8 +13,7 @@
         <ul
           class="listStyle_none bg-gray bg_lg_transparent
           zIndex_10 position_absolute position_lg_relative
-          align-items-center d-lg-flex m-0 p-0 pt-2 pb-3 p-lg-0 list_position"
-        >
+          align-items-center d-lg-flex m-0 p-0 pt-2 pb-3 p-lg-0 list_position">
           <li class="py-3 py-lg-0">
             <router-link class="text-black px-3 mr-2 text-decoration-none list_hover"
             to="/products">Products</router-link>
@@ -46,11 +46,27 @@
   </div>
 </template>
 
+<script>
+/* global $ */
+
+export default {
+  methods: {
+    navbarShow() {
+      $('.list_position').slideToggle('fast');
+    },
+  },
+  created() {
+    this.navbarShow();
+  },
+};
+</script>
+
 <style lang="scss">
 .list_position {
-  top: 77px;
+  top: 62px;
   width: 100%;
   left: 0;
+  display: none;
   @media (min-width: 992px) {
     top: 0;
   }
