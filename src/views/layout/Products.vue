95b5550add499ae3e18024098905221f5e4218f6
juker1141 <50107434+juker1141@users.noreply.github.com>
2020-09-20 21:10:22 +0800

    update

--- src/views/layout/Products.vue
+++ src/views/layout/Products.vue
@@ -4,19 +4,19 @@
     data-ride="carousel">
       <div class="carousel-inner">
         <div class="carousel-item active">
-          <div class="bg-danger py-3 text-white fz_24">
+          <div class="bg-danger py-3 text-white fz_lg_24">
           開幕限時特價，全品項77折，快輸入 ' HAPPY777 ' 吧！</div>
         </div>
         <div class="carousel-item">
-          <div class="bg-dark py-3 text-yellow fz_24">
+          <div class="bg-dark py-3 text-yellow fz_lg_24">
             想要獲得更多活動訊息及優惠資訊嗎？快訂閱我們的會員電子報吧！</div>
         </div>
         <div class="carousel-item">
-          <div class="bg-danger py-3 text-white fz_24">
+          <div class="bg-danger py-3 text-white fz_lg_24">
           開幕限時特價，全品項77折，快輸入 ' HAPPY777 ' 吧！</div>
         </div>
         <div class="carousel-item">
-          <div class="bg-dark py-3 text-yellow fz_24">
+          <div class="bg-dark py-3 text-yellow fz_lg_24">
             想要獲得更多活動訊息及優惠資訊嗎？快訂閱我們的會員電子報吧！</div>
         </div>
       </div>
