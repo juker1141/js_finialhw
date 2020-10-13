1379027c19698052154ba6e7281aebd85499eb18
juker <juker1141@gmail.com>
2020-10-13 17:22:34 +0800

    update

--- src/views/frontend/About.vue
+++ src/views/frontend/About.vue
@@ -54,8 +54,7 @@
     <div class="modal-dialog modal-dialog-centered w_max_md_80" role="document">
       <div class="modal-content bg-blackOP text-white">
         <div class="modal-header border-0">
-          <h5 class="modal-title" id="exampleModalLabel">
-          <span class="fontOrbitron ml-2">Hardware Store</span></h5>
+          <h5 class="modal-title" id="exampleModalLabel"></h5>
           <button type="button" class="btn
           m-0 p-0 d-flex align-items-center close opacity_1"
           data-dismiss="modal" aria-label="Close">
@@ -63,24 +62,45 @@
           </button>
         </div>
         <div class="modal-body">
-          <div class="">
-            <div class="row w-50">
-              <div class="col-6"><div class="aboutUsImg aboutUsImg_1"></div></div>
-              <div class="col-6">
-                <div class="d-flex flex-column">
-                  <div class="row">
-                    <div class="col-12"><div class="aboutUsImg aboutUsImg_2"></div></div>
+          <div class="row mx-5">
+            <div class="col-7">
+              <div class="d-flex">
+                <div class="w-50 mr-3">
+                  <div class="h-100 d-flex flex-column">
+                    <div class="h-75 w-100 mb-3">
+                      <div class="h-100 aboutUsImg aboutUsImg_2"></div></div>
+                    <div class="h-25 w-100">
+                      <div class="h-100 aboutUsImg aboutUsImg_3"></div></div>
                   </div>
-                  <div class="row">
-                    <div class="col-12"><div class="aboutUsImg aboutUsImg_3"></div></div>
+                </div>
+                <div class="w-50"><div class="h_550px aboutUsImg aboutUsImg_1"></div></div>
+              </div>
+            </div>
+            <div class="col-5 h_550px">
+              <div class="d-flex h-100 flex-column justify-content-between">
+                <div class="d-flex h-100 flex-column justify-content-center align-items-center">
+                  <div class="mb-3 fz_24">從 1990 年代起</div>
+                  <div class="mb-3"><span class="fontOrbitron">Hardware Store</span> 就在高雄發跡</div>
+                  <div class="mb-3">從一開始10坪大的五金行做到現在的五金批發業</div>
+                  <div class="mb-3">就在今年也加入網路這個大家族了！</div>
+                  <div class="mb-3">專門販售各品牌的手工具</div>
+                  <div class="mb-3">電動工具、氣動工具以及其配件、耗材</div>
+                  <div class="mb-3">我們也常配合企業採購及政府標案</div>
+                  <div class="mb-3">如需大量採購煩請填寫以下表單</div>
+                  <div>我們將會在最短時間內與您聯繫，謝謝！</div>
+                </div>
+                <div class="d-flex align-items-center justify-content-end">
+                  <div class="btn text-white border-white rounded-0 d-flex align-items-center
+                  p-2 px-5 formBtn_hover">
+                    填寫表單
+                    <span class="material-icons fz_24 pl-2 text-black d-none formBtn_hover_arrow">
+                    navigate_next
+                    </span>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-          <p>從 1990 年代起， <span class="fontOrbitron">Hardware Store</span> 就開始經營五金批發<br>
-          對於各品牌的手工具、電動工具、氣動工具更是瞭若指掌<br>
-          若想了解更多資訊，歡迎直接聯繫我們！</p>
         </div>
         <div class="modal-footer border-0">
         </div>
@@ -137,17 +157,21 @@
   background-position: 50% 75% !important;
   background: url(https://hexschool-api.s3.us-west-2.amazonaws.com/custom/BIzqAYnOJ9VVk2wUTGxVFyXvUVGvm52beZUQW70jAvDwSHXDLvyjek62b2CvLl7gIlKAHEsOn3sORffJHPVIZ2B0QZkyL2GdTU39A0cgmUMAWyOJ0OHezA1iqYAipAYf.jpg) no-repeat;
   filter: sepia(.6);
-  height: 550px;
 }
 .aboutUsImg_2{
   background-position: center !important;
   background: url(https://hexschool-api.s3.us-west-2.amazonaws.com/custom/8yGbq2NuwZnfADDZSayJYILMsnHoDGAvEcnRmaJO8yarICrgiHwPhERBUggeuxiV5WtJYiXKJENstxWrwEtVz7TIyRcR5eCaNsZFuZUEnnXXT3WPbI24NZ3Wk63h0Tl9.jpg) no-repeat;
-  height: 265px;
-  margin-bottom: 20px;
 }
 .aboutUsImg_3{
   background-position: center !important;
   background: url(https://hexschool-api.s3.us-west-2.amazonaws.com/custom/9BjrmSggbENmkk9k4IxX6lSsSlvZV7FZheaZhbfznaxHyFkK01AWV4jPu655fDTmCQts6DmxfMuJOe53XSrEahRKb1ArIaBYHL4iJ9rc2nsPOQ7m0RDL5FUzqwH3aWX7.jpg) no-repeat;
-  height: 265px;
 }
+.formBtn_hover:hover{
+  color: #000 !important;
+  background: #fff;
+  .formBtn_hover_arrow{
+    display: inline-block !important;
+  }
+}
+
 </style>
